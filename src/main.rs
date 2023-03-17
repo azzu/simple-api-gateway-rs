589577738adebc38e9c471ce4cbf98ca2cf59a31
jeongillee <ji@sk.com>
2023-03-17 23:31:19 +0900

    set default value and overwrite CLI argument value if exists.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
-use std::path::PathBuf;
 use clap::Parser;
+
+struct Config {
+    host: String,
+    port: u16,
+    config: String,
+    debug: u8,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            host: "localhost".to_owned(),
+            port: 8888,
+            config: "".to_string(),
+            debug: 0
+        }
+    }
+
+    fn set_args_val(&mut self, args: &Args) -> &mut Self {
+        match &args.host {
+            Some(v) => self.host = v.to_string(),
+            _ => {}
+        }
+        match &args.port {
+            Some(v) => self.port = *v,
+            _ => {}
+        }
+        match &args.config {
+            Some(v) => self.config = v.to_string(),
+            _ => {}
+        }
+        match &args.debug {
+            Some(v) => self.debug = *v,
+            _ => {}
+        }
+        self
+    }
+}
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
+    #[arg(long)]
+    host: Option<String>,
+
+    #[arg(long)]
+    port: Option<u16>,
 
     #[arg(short, long, value_name = "CONFIG_PATH")]
-    config: Option<PathBuf>,
+    config: Option<String>,
 
     #[arg(short)]
     debug: Option<u8>
@@ -17,7 +57,9 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    if let _is_name_exist = args.name.len() > 0 {
-        println!("Hello, {}!", args.name);
-    }
+    let mut config = Config::new();
+
+    config.set_args_val(&args);
+
+    println!("{}:{}", config.host, config.port);
 }
